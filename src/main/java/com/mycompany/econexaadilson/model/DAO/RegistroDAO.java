db1072cf17580ee1a936f1fbea2f5d9aab7acc3b
Gustavo de Freitas Andrade <gu.fre.and@gmail.com>
2025-10-13 21:22:57 -0300

    Adicionando classes principais

---
+++ src/main/java/com/mycompany/econexaadilson/model/DAO/RegistroDAO.java
@@ -0,0 +1,14 @@
+package com.mycompany.econexaadilson.model.DAO;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author gufre
+ */
+public class RegistroDAO {
+    
+}
